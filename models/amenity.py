475e497a21cea92b22666ad40257d00cab0625a4
ShadrackOdielo <shadrackodielo@gmail.com>
2023-06-21 23:09:38 +0300

    transfer from file 2

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,26 @@
-#!/usr/bin/python
-""" holds class Amenity"""
-import models
-from models.base_model import BaseModel, Base
+#!/usr/bin/python3
+
+"""
+A module that defines the ORM class for Amenity table
+"""
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
+from models.base_model import Base, BaseModel
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+    """
+    Defines Amenity class attributes
+    """
+    __tablename__ = 'amenities'
 
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(60), nullable=False)
+        place_amenities = relationship(
+            'Place', secondary=place_amenity, viewonly=False
+        )
+    else:
+        name = ''
--- models/review.py
+++ models/review.py
@@ -1,24 +1,27 @@
-#!/usr/bin/python
-""" holds class Review"""
-import models
-from models.base_model import BaseModel, Base
+#!/usr/bin/python3
+
+"""
+A module that defines ORM class for Review table
+"""
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import String
+from models.base_model import Base, BaseModel
 
 
 class Review(BaseModel, Base):
-    """Representation of Review """
-    if models.storage_t == 'db':
-        __tablename__ = 'reviews'
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    """
+    Defines attributes for Review table
+    """
+    __tablename__ = 'reviews'
 
-    def __init__(self, *args, **kwargs):
-        """initializes Review"""
-        super().__init__(*args, **kwargs)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        text = Column(String(1024), nullable=False)
+        place_id = Column(
+            String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(
+            String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        text = ''
+        place_id = ''
+        user_id = ''
